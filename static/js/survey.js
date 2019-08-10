c4c0db09f54bdc5909f8de7bb951a44e72518abb
Veraj Paruthi <veraj.paruthi@gmail.com>
2019-08-09 22:42:56 -0400

    Implemeting bootstrap form-grid.

--- static/js/survey.js
+++ static/js/survey.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
-    var bootstrap = require('bootstrap');
-
+    console.log(questions)
+    var currentQuestionIndex = 0
+    var formQuestions = $('form').children('div.question')
+//    formQuestions.hide()
+//    formQuestions.eq(currentQuestionIndex).show()
+    $('.next-btn').click(function(){
+        formQuestions.eq(0).find('h3').text(formQuestions.eq(currentQuestionIndex+1).find('h3').text())
+        currentQuestionIndex++
+    });
 });
