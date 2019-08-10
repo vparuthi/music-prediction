24df92fd97d53e6c9d1a36c446768163b7406ce8
Veraj Paruthi <veraj.paruthi@gmail.com>
2019-08-10 15:49:00 -0400

    Pivoted from using form to buttons. Changed all html and js code.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import preprocessing
 import model.mlknn
 import json
 import pickle
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, current_app
 import csv
 from itertools import compress
 import requests
@@ -17,29 +17,25 @@ SPOTIFY_SEARCH_API_URL = 'https://api.spotify.com/v1/search?q='
 CONFIG_FILE_PATH = 'config.ini'
 DEFAULT_NUMBER_OF_QUESTION_RESPONSE_OPTIONS = 5
 # value is the question, key is the number of categories
-CATEGORICAL_QUESTIONS = {'Gender': 2}
+CATEGORICAL_QUESTIONS = {'Gender': ['Male', 'Female']}
 app = Flask(__name__)
 
 
 @app.route('/process_survey', methods=['GET'])
 def process():
     form_values = request.args
-    clf = pickle.load(open(FINAL_MLKNN_MODEL_FILE_PATH, 'rb'))
-    # predicted_values = model.mlknn.predict(clf, list(form_values.values()))
-    # with open(OPTIMIZED_MODEL_PARAMETERS_FILE_PATH) as file:
-    #     genres = json.load(file)['genres']
-    # genres = list(compress(genres, predicted_values))
+    predicted_values = model.mlknn.predict(current_app.model, list(form_values.values()))
+    genres = list(compress(current_app.genres, predicted_values))
+
     config = configparser.ConfigParser()
     config.read(CONFIG_FILE_PATH)
     spotify_api_keys = config['spotify api']
-
     client_credentials_manager = SpotifyClientCredentials(client_id=spotify_api_keys['CLIENT_ID'],
                                                           client_secret=spotify_api_keys['CLIENT_SECRET'])
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
-
-    spotify_api_results = {'hip hop': sp.search(q='hip hop', type='playlist', limit=6)}
-    # for genre in genres:
-    #     spotify_api_results[genre] = sp.search(q=genre, type='playlist', limit=6)
+    spotify_api_results = {}
+    for genre in genres:
+        spotify_api_results = {genre: sp.search(q=genre, type='playlist', limit=6)}
 
     return render_template('results.html', genres=spotify_api_results)
 
@@ -51,13 +47,13 @@ def index():
     with open('./resources/columns.csv', mode='r') as infile:
         reader = csv.reader(infile)
         question_correlations = dict((rows[1], rows[0]) for rows in reader)
-    questions = [question_correlations[question.capitalize()] for question in questions]
+    questions = [question_correlations[question.capitalize().replace('.', '')] for question in questions]
     questions_and_options = []
     for question in questions:
         if question in CATEGORICAL_QUESTIONS:
             questions_and_options.append([question, CATEGORICAL_QUESTIONS[question]])
         else:
-            questions_and_options.append([question, DEFAULT_NUMBER_OF_QUESTION_RESPONSE_OPTIONS])
+            questions_and_options.append([question, list(range(DEFAULT_NUMBER_OF_QUESTION_RESPONSE_OPTIONS+1))])
 
     return render_template('survey.html', questions=questions_and_options)
 
@@ -75,8 +71,12 @@ def create_new_model():
 
 
 def main():
+    mlknn_clf = pickle.load(open(FINAL_MLKNN_MODEL_FILE_PATH, 'rb'))
+    with open(OPTIMIZED_MODEL_PARAMETERS_FILE_PATH) as file:
+        genres = json.load(file)['genres']
+    app.model = mlknn_clf
+    app.genres = genres
     app.run(debug=True)
-    # load the model from disk
 
 if __name__ == '__main__':
     main()
--- static/js/survey.js
+++ static/js/survey.js
@@ -1,76 +1,78 @@
 $(document).ready(function(){
 //    questions is a 2D array, where col_1 = question text and col_2 = number of response options
     var currentQuestionIndex = 0
-    var form = $('form')
+    var form = $('.form')
     var questionTitle = form.find('h3')
     var formButtonsDiv = form.find('div.form-check-inline')
     var currentQuestion = questions[currentQuestionIndex]
     var responseValues = {}
+    var submitButton = $('.submit-btn')
+    var nextButton = $('.next-btn')
+    var previousButton = $('.previous-btn')
 
     populateForm(formButtonsDiv, currentQuestion, questionTitle)
 
-    $('.next-btn').click(function(){
-        if (currentQuestionIndex == questions.length - 1){
-            $(this).hide()
-        }
-        if (checkIfSelected(form, formButtonsDiv)){
-            responseValues[currentQuestionIndex] = $('input:checked', formButtonsDiv).val()
+    $('.response-btn').click(function(){
+        $('.response-btn').removeClass('btn-selected')
+        $(this).addClass('btn-selected')
+        responseValues[currentQuestionIndex] = $(this).val()
+        console.log(responseValues)
+    })
+
+    $(nextButton).click(function(){
+        console.log(currentQuestionIndex)
+        if (checkIfSelected(formButtonsDiv, responseValues, currentQuestionIndex)){
             currentQuestionIndex++;
+            if (currentQuestionIndex + 1 == questions.length){
+                $(this).css('visibility', 'hidden')
+            }
             currentQuestion = questions[currentQuestionIndex]
-            formButtonsDiv.empty()
-            populateForm(formButtonsDiv, currentQuestion, questionTitle)
-            console.log(responseValues)
-            $('.previous-btn').show()
+            updateForm(questionTitle, currentQuestion, responseValues, currentQuestionIndex)
         }
-        reselectResponse(responseValues, currentQuestionIndex)
+        $('.previous-btn').css('visibility', 'visible')
     });
 
-    $('.previous-btn').click(function(){
-        responseValues[currentQuestionIndex] = $('input:checked', formButtonsDiv).val()
-
+    previousButton.click(function(){
         currentQuestionIndex--
         if (currentQuestionIndex == 0){
-            $(this).hide()
+            $(this).css('visibility', 'hidden')
         }
         currentQuestion = questions[currentQuestionIndex]
-
-        formButtonsDiv.empty()
-        populateForm(formButtonsDiv, currentQuestion, questionTitle)
-        console.log(responseValues)
-
-        reselectResponse(responseValues, currentQuestionIndex)
+        updateForm(questionTitle, currentQuestion, responseValues, currentQuestionIndex)
+        $('.next-btn').css('visibility', 'visible')
     });
 });
 
-function checkIfSelected(form, formButtonsDiv){
-    if ($('input:radio', form).is(':checked')) {
-        return true;
-    } else {
-        $(formButtonsDiv).effect('shake')
-        return false;
+function checkIfSelected(formButtonsDiv, responseValues, currentQuestionIndex){
+    if (typeof responseValues[currentQuestionIndex] != 'undefined'){
+        return true
     }
+    $(formButtonsDiv).effect("shake")
+    return false
 }
 
 function populateForm(formButtonsDiv, currentQuestion, questionTitle){
-    for (var option = 1; option <= currentQuestion[1]; option++){
-        $('<label/>', {
-            label: option,
-            text: option.toString(),
-            class: 'question-label'
-        }).appendTo(formButtonsDiv);
-        $('<input/>', {
-            type: 'radio',
-            name: currentQuestion[0],
-            id: option,
+    for (option in currentQuestion[1]){
+        $('<button/>', {
             value: option,
-            class: 'question-input'
+            text: option.toString(),
+            class: 'btn btn-info btn-circle btn-xl response-btn',
+            id: option
         }).appendTo(formButtonsDiv);
     }
     questionTitle.text(currentQuestion[0])
 }
 
 function reselectResponse(responseValues, currentQuestionIndex){
-    if (typeof responseValues[currentQuestionIndex] != 'undefined' ){
-        $("#" + (responseValues[currentQuestionIndex]).toString()).prop("checked", true);
+    if (typeof responseValues[currentQuestionIndex] != 'undefined'){
+        responseSelected = responseValues[currentQuestionIndex]
+        $("button[value='"+responseSelected.toString()+"']").addClass('btn-selected')
     }
+    return
+}
+
+function updateForm(questionTitle, currentQuestion, responseValues, currentQuestionIndex){
+    questionTitle.text(currentQuestion[0])
+    $('.response-btn').removeClass('btn-selected')
+    reselectResponse(responseValues, currentQuestionIndex)
 }
