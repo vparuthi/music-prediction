5f958ff709b3697bfc45ca6daa2a8d2aa6891996
Veraj <veraj.paruthi@gmail.com>
2019-09-09 01:34:08 -0400

    - Started working on implementing GCP CloudSQL - Added form buttons on results.html page

--- static/js/results.js
+++ static/js/results.js
@@ -1,5 +1,10 @@
 define(['domReady', 'jquery', 'jqueryUI'], function(domReady, $, ui, ){
     domReady(function(){
+        var responseValues = {};
+        var genres = $('.form-buttons').find('.response-btn');
+        for(var i=0; i < genres.length; i++){
+            responseValues[$(genres[i]).text()] = -1
+        }
         $(".animation").each(function (i) {
         // store the item around for use in the 'timeout' function
         var $item = $(this);
@@ -9,5 +14,15 @@ define(['domReady', 'jquery', 'jqueryUI'], function(domReady, $, ui, ){
         }, 500*i);
         // each element should animate half a second after the last one.
       });
+        $('.response-btn').click(function(){
+            if ($(this).hasClass('btn-selected')){
+                $(this).removeClass('btn-selected')
+                responseValues[$(this).text()] = -1
+            }else{
+                $(this).addClass('btn-selected')
+                responseValues[$(this).text()] = 1
+            }
+            $('.input-text').val(JSON.stringify(responseValues))
+        })
     });
 });
